7ee9ed3dceb31011269cf42eb673b7440da8274e
Ali Shah <s.ashah@outlook.com>
2020-12-04 19:54:15 -0500

    [Update] Finished detail.html and relevant app routes
    - Adding a new plant works
    - Harvest form works

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 """WEB1.1 Assignment 5: Databases."""
 
 from bson.objectid import ObjectId
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 
 ############################################################
@@ -13,8 +13,11 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/plantdb"
 mongo = PyMongo(app)
+
+plants = mongo.db.plants
+harvests = mongo.db.harvests
 
 ############################################################
 # ROUTES
@@ -23,14 +26,7 @@ mongo = PyMongo(app)
 @app.route('/')
 def plants_list():
     """Display the plants list page."""
-
-    # TODO: Replace the following line with a database call to retrieve *all*
-    # plants from the Mongo database's `plants` collection.
-    plants_data = ''
-
-    context = {
-        'plants': plants_data,
-    }
+    context = {'plants': plants.find()}
     return render_template('plants_list.html', **context)
 
 @app.route('/about')
@@ -41,60 +37,45 @@ def about():
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     """Display the plant creation page & process data from the creation form."""
-    if request.method == 'POST':
-        # TODO: Get the new plant's name, variety, photo, & date planted, and 
-        # store them in the object below.
-        new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
-        }
-        # TODO: Make an `insert_one` database call to insert the object into the
-        # database's `plants` collection, and get its inserted id. Pass the 
-        # inserted id into the redirect call below.
 
-        return redirect(url_for('detail', plant_id=''))
-
-    else:
+    if request.method == 'GET':
         return render_template('create.html')
-
-@app.route('/plant/<plant_id>')
-def detail(plant_id):
-    """Display the plant detail page & process data from the harvest form."""
-
-    # TODO: Replace the following line with a database call to retrieve *one*
-    # plant from the database, whose id matches the id passed in via the URL.
-    plant_to_show = ''
-
-    # TODO: Use the `find` database operation to find all harvests for the
-    # plant's id.
-    # HINT: This query should be on the `harvests` collection, not the `plants`
-    # collection.
-    harvests = ''
-
-    context = {
-        'plant' : plant_to_show,
-        'harvests': harvests
+    # else:  # if method == 'POST':
+    new_plant = {
+        'name': request.form.get('plant_name'),
+        'variety': request.form.get('variety'),
+        'photo_url': request.form.get('photo'),
+        'date_planted': request.form.get('date_planted')
     }
-    return render_template('detail.html', **context)
+
+    plant_id = plants.insert_one(new_plant).inserted_id
+
+    return redirect(url_for('detail', plant_id=plant_id))
 
 @app.route('/harvest/<plant_id>', methods=['POST'])
 def harvest(plant_id):
     """Accepts a POST request with data for 1 harvest and inserts into database."""
 
-    # TODO: Create a new harvest object by passing in the form data from the
-    # detail page form.
     new_harvest = {
-        'quantity': '', # e.g. '3 tomatoes'
-        'date': '',
+        'quantity': request.form.get('harvested_amount'),
+        'date': request.form.get('date_planted'),
         'plant_id': plant_id
     }
 
-    # TODO: Make an `insert_one` database call to insert the object into the 
-    # `harvests` collection of the database.
+    harvests.insert_one(new_harvest)
 
     return redirect(url_for('detail', plant_id=plant_id))
+
+@app.route('/plant/<plant_id>')
+def detail(plant_id):
+    """Display the plant detail page & process data from the harvest form."""
+    plant_to_show = plants.find_one({'_id': ObjectId(plant_id)})
+    harvest_list = harvests.find()
+    context = {
+        'plant' : plant_to_show,
+        'harvests': harvest_list
+    }
+    return render_template('detail.html', **context)
 
 @app.route('/edit/<plant_id>', methods=['GET', 'POST'])
 def edit(plant_id):
